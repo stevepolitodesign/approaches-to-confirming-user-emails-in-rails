30d7b36935c6fd8b77d2a90e3932a25186830190
Steve Polito <stevepolito@hey.com>
2023-01-15 09:05:26 -0500

    Improve security with expiration and purpose
    This is better, but the token could still be used again before the
    expiration.

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,6 +1,6 @@
 class ConfirmationsController < ApplicationController
   def edit
-    @user = User.unconfirmed.find_signed!(params[:signed_id])
+    @user = User.unconfirmed.find_signed!(params[:signed_id], purpose: :confirmation)
     @user.update!(confirmed_at: Time.current)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      signed_id = @user.signed_id
+      signed_id = @user.signed_id expires_in: 15.minutes, purpose: :confirmation
       redirect_to user_path(@user, params: { signed_id: signed_id }),  notice: "Confirm your account"
     else
       flash.now.alert =  @user.errors.full_messages.to_sentence
